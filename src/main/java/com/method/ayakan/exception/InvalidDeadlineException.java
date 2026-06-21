b4cf7963a346cdbddeb46b5eb7099600eb1d79b2
intan <intancintya3@gmail.com>
2026-06-21 20:45:07 +0700

    benerin dikit commit sebelumnya

---
+++ src/main/java/com/method/ayakan/exception/InvalidDeadlineException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.method.ayakan.exception;
+
+/**
+ *
+ * @author Cintya
+ */
+public class InvalidDeadlineException extends Exception {
+    public InvalidDeadlineException(String message) {
+        super(message);
+    }
+}
+
