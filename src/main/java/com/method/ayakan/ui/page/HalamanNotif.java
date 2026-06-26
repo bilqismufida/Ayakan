8e57b81b1ed8b556d4d16f9938bd579801a6a1fe
karubeef <nayaka2510@gmail.com>
2026-06-26 15:29:32 +0700

    memperbaiki notif

--- src/main/java/com/method/ayakan/ui/page/HalamanNotif.java
+++ src/main/java/com/method/ayakan/ui/page/HalamanNotif.java
@@ -132,7 +132,6 @@ public class HalamanNotif {
             System.out.printf("| %-64s |\n", text.substring(0, batas));
             
             text = text.substring(batas).trim();
-            
         }
 
         System.out.printf("| %-64s |\n", text);
